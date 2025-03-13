eeef431e8c63e938b3b96647614cee03e7398c67
Carolyne Golias <goliascarolyne@gmail.com>
2025-03-13 20:19:30 +0000

    updated code in app tsx for my google maps

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,29 @@
+import {
+  APIProvider,
+  Map,
+  MapCameraChangedEvent,
+} from '@vis.gl/react-google-maps';
 import './App.css';
-
-export default function App() {}
+const googleKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+export default function App() {
+  return (
+    <>
+      <APIProvider
+        apiKey={googleKey}
+        onLoad={() => console.log('maps api has loaded')}>
+        <Map
+          defaultZoom={13}
+          defaultCenter={{ lat: -33.860664, lng: 151.208138 }}
+          onCameraChanged={(ev: MapCameraChangedEvent) =>
+            console.log(
+              'camera changed:',
+              ev.detail.center,
+              'zoom:',
+              ev.detail.zoom
+            )
+          }
+        />
+      </APIProvider>
+    </>
+  );
+}

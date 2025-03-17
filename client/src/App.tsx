17cc0428ee50f89a0ce5b97cfb641abcf0347ff8
Carolyne Golias <goliascarolyne@gmail.com>
2025-03-17 17:40:41 +0000

    updating routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,17 @@
 import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import { ProgressAssessment } from './components/ProgressAssessment.tsx';
 import { TherapyAssessment } from './components/TherapyAssessment.tsx';
 
 export default function App() {
   return (
     <>
-      <TherapyAssessment />
-      <ProgressAssessment />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<TherapyAssessment />} />
+        <Route path="progress" element={<ProgressAssessment />} />
+      </Routes>
     </>
   );
 }

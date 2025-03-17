0902f4c5c058dc9722cc2c03226937b8ff7f6468
Carolyne Golias <goliascarolyne@gmail.com>
2025-03-17 23:05:50 +0000

    started work on locate page but change my mind

--- client/src/pages/UserPage.tsx
+++ client/src/pages/UserPage.tsx
@@ -1,25 +1,13 @@
 import './UserPage.css';
 export function UserPage() {
   return (
-    <div className="logo-container">
-      <div className="row">
-        <div className="column-one">
-          <img src="/public/TherapyLogo.png" className="logo"></img>
-        </div>
-      </div>
-      <div className="row">
-        <div className="column-one">
-          <button className="signIn">Sign In</button>
-          <button className="signUp">Sign Up</button>
-          <button className="locate">Locate</button>
-        </div>
-      </div>
+    <>
       <div className="body-row">
         <div className="column-two">
           <p>Welcome, User name</p>
           <div className="placeholder-chart"></div>
         </div>
       </div>
-    </div>
+    </>
   );
 }

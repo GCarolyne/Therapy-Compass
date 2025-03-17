f34427da92179bf263c28edb1841aa6520971295
Carolyne Golias <goliascarolyne@gmail.com>
2025-03-17 00:52:14 +0000

    checking for dependencies in main

---
+++ client/src/components/LineChart.tsx
@@ -0,0 +1,17 @@
+// import { LineChart } from '@mui/x-charts';
+
+// export function LineChart() {
+//   return (
+//     <>
+//       <LineChart
+//         width={500}
+//         height={300}
+//         series={[
+//           { data: pData, label: 'pv' },
+//           { data: uData, label: 'uv' },
+//         ]}
+//         xAxis={[{ scaleType: 'point', data: xLabels }]}
+//       />
+//     </>
+//   );
+// }

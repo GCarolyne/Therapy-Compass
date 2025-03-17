15d6290818e27889e44b9f70c0783a38f05f8d36
Carolyne Golias <goliascarolyne@gmail.com>
2025-03-17 22:46:38 +0000

    completed

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -3,29 +3,15 @@ import './SignUp.css';
 export function SignUp() {
   return (
     <>
-      <div className="logo-container">
-        <div className="row">
-          <div className="column-one">
-            <img src="/public/TherapyLogo.png" className="logo"></img>
-          </div>
-        </div>
-        <div className="row">
-          <div className="column-one">
-            <button>Sign In</button>
-            <button>Sign Up</button>
-            <button>Locate</button>
-          </div>
-        </div>
-        <div className="body-row">
-          <div className="column-two">
-            <label>
-              UserName
-              <input type="text"></input>
-            </label>
-            <label>
-              <input type="text"></input>
-            </label>
-          </div>
+      <div className="body-row">
+        <div className="column-two">
+          <label>
+            UserName
+            <input type="text"></input>
+          </label>
+          <label>
+            <input type="text"></input>
+          </label>
         </div>
       </div>
     </>

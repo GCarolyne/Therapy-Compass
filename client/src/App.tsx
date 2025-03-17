f2be0892847420820dd25ebafc99e82f32cb3d6e
Carolyne Golias <goliascarolyne@gmail.com>
2025-03-17 20:37:26 +0000

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,15 @@
 import './App.css';
 import { UserPage } from './pages/UserPage';
 
-// import { ProgressAssessment } from './components/ProgressAssessment.tsx';
-// import { TherapyAssessment } from './components/TherapyAssessment.tsx';
+import { ProgressAssessment } from './components/ProgressAssessment.tsx';
+import { TherapyAssessment } from './components/TherapyAssessment.tsx';
 
 export default function App() {
   return (
     <>
       <UserPage />
-      {/* <TherapyAssessment />
-      <ProgressAssessment /> */}
+      <TherapyAssessment />
+      <ProgressAssessment />
     </>
   );
 }

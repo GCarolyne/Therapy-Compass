42978c2fc30f1a5cd5f107d1c17c6c7bb289600e
Carolyne Golias <goliascarolyne@gmail.com>
2025-03-17 22:23:16 +0000

    completed headerfor pages

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,26 +3,28 @@ import { Link } from 'react-router-dom';
 import './Header.css';
 export function Header() {
   return (
-    <div className="logo-container">
+    <>
       <div className="row">
         <div className="column-one">
-          <img src="/public/TherapyLogo.png" className="logo"></img>
-
-          <Link to="/Signin" className="text-white">
-            <button>Sign In</button>
-          </Link>
-
-          <Link to="/Signup" className="text-white">
-            <button>Sign Up</button>
-          </Link>
-
-          <Link to="/Locate" className="text-white">
-            <button>Locate</button>
-          </Link>
-
-          <Outlet />
+          <img
+            src="/public/TherapyLogo.png"
+            className="logo"
+            alt="Therapy Logo"
+          />
+          <div className="nav-links">
+            <Link to="/Signin">
+              <button>Sign In</button>
+            </Link>
+            <Link to="/Signup">
+              <button>Sign Up</button>
+            </Link>
+            <Link to="/Locate">
+              <button>Locate</button>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+      <Outlet />
+    </>
   );
 }

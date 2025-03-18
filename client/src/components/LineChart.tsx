a5887c05780fe69d14fdcbfbfeea1e03cbb9d24b
Carolyne Golias <goliascarolyne@gmail.com>
2025-03-18 03:35:49 +0000

    started working on line chart

--- client/src/components/LineChart.tsx
+++ client/src/components/LineChart.tsx
@@ -1,3 +1,31 @@
-export function LineChart() {
-  return <></>;
-}
+// import { Line } from 'react-chartjs-2';
+// import {
+//   Chart as ChartJS,
+//   CategoryScale,
+//   LinearScale,
+//   PointElement,
+//   LineElement,
+//   Title,
+//   Tooltip,
+//   Legend,
+// } from 'chart.js';
+
+// ChartJS.register(
+//   CategoryScale,
+//   LinearScale,
+//   PointElement,
+//   LineElement,
+//   Title,
+//   Tooltip,
+//   Legend
+// );
+
+// export function LineChart() {
+//   const options = {};
+//   const data = { lineChartData };
+//   return (
+//     <>
+//       Line options={} data={}
+//     </>
+//   );
+// }
--- database/data.sql
+++ database/data.sql
@@ -9,3 +9,16 @@
 --      ('Learn to code', false),
 --      ('Build projects', false),
 --      ('Get a job', false);
+
+export const lineChartData = {
+labels:[
+  "Week 1","Week 2","Week3","Week4"
+],
+datasets: [
+  {
+    label:"Wellness Score",
+    data: [10,20,30,40],
+    borderColor:"rgb(75,192,234)"
+  }
+]
+}
